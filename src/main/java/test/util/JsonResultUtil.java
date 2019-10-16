f040772e6dfa2693ff22700002637630481351eb
冰生于水 <487005831@qq.com>
2019-10-16 15:28:16 +0800

    修改返回Map对象共享的方式

--- src/main/java/test/util/JsonResultUtil.java
+++ src/main/java/test/util/JsonResultUtil.java
@@ -1,9 +1,7 @@
 package test.util;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @作者：刘时明
@@ -12,82 +10,48 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class JsonResultUtil
 {
-    private static Map<String,Object> map=new ConcurrentHashMap<>();
-    private static Lock lock=new ReentrantLock();
-
-    public static Map<String,Object> susses(int code,String susses)
+    public static Map<String, Object> susses(int code, String susses)
     {
-        try
-        {
-            lock.lock();
-            map.clear();
-            map.put("code",code);
-            map.put("susses",susses);
-            return map;
-        }finally
-        {
-            lock.unlock();
-        }
+        Map<String, Object> map = new HashMap();
+        map.clear();
+        map.put("code", code);
+        map.put("susses", susses);
+        return map;
     }
 
-    public static Map<String,Object> error(int code,String error)
+    public static Map<String, Object> error(int code, String error)
     {
-        try
-        {
-            lock.lock();
-            map.clear();
-            map.put("code",code);
-            map.put("error",error);
-            return map;
-        }finally
-        {
-            lock.unlock();
-        }
+        Map<String, Object> map = new HashMap();
+
+        map.clear();
+        map.put("code", code);
+        map.put("error", error);
+        return map;
     }
 
-    public static Map<String,Object> of(Object data)
+    public static Map<String, Object> of(Object data)
     {
-        try
-        {
-            lock.lock();
-            map.clear();
-            map.put("data",data);
-            return map;
-        }finally
-        {
-            lock.unlock();
-        }
+        Map<String, Object> map = new HashMap();
+        map.clear();
+        map.put("data", data);
+        return map;
     }
 
-    public static Map<String,Object> of(int code,String susses,String error)
+    public static Map<String, Object> of(int code, String susses, String error)
     {
-        try
-        {
-            lock.lock();
-            map.clear();
-            map.put("code",code);
-            map.put("susses",susses);
-            map.put("error",error);
-            return map;
-        }finally
-        {
-            lock.unlock();
-        }
+        Map<String, Object> map = new HashMap();
+        map.put("code", code);
+        map.put("susses", susses);
+        map.put("error", error);
+        return map;
     }
 
-    public static Map<String,Object> of(int code, String susses,Object data)
+    public static Map<String, Object> of(int code, String susses, Object data)
     {
-        try
-        {
-            lock.lock();
-            map.clear();
-            map.put("code",code);
-            map.put("susses",susses);
-            map.put("data",data);
-            return map;
-        }finally
-        {
-            lock.unlock();
-        }
+        Map<String, Object> map = new HashMap();
+        map.put("code", code);
+        map.put("susses", susses);
+        map.put("data", data);
+        return map;
     }
 }
